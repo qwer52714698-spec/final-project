88221b9277f492d14efdffe2281f26895aaf7edf
커블 <qwer52714698@gmail.com>
2026-05-19 10:13:27 +0900

    5/19 백엔드 배포

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://final-project-56uc.onrender.com',
+  timeout: 60000, // 
   headers: {
-    'Content-Type': 'application/json',
-  },
-})
+    'Content-Type': 'application/json'
+  }
+});
 
-export default api
+export default instance;
